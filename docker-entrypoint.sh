37819e5b503ab67e0f20befd29ab6d2992673e84
Aurelian Dumanovschi <aurasd@gmail.com>
2017-10-20 17:09:33 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-rm credentials.json
-ln -s /run/secrets/credentials.json credentials.json
-
 if [ "${1:0:1}" = '-' ]; then
   set -- /google-speedtest-char/speedtest-charts.py "$@"
 fi
